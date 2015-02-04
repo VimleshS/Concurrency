a1677f613bab0161d6c4ee19413d231f6149cf66
Vimlesh Sharma <vimlesh.sharma@drillinginfo.com>
2015-02-04 16:46:14 +0530

    Sequencer on FanIn method

--- Concurrency.go
+++ Concurrency.go
@@ -6,18 +6,29 @@ import (
 	"time"
 )
 
+type Message struct {
+	msg  string
+	wait chan bool
+}
+
 func main() {
-	//c := boring("Vimlesh")
 	c := fanIn(boring("Ann"), boring("Joe"))
-	for i := 0; i <= 5; i++ {
-		fmt.Println(<-c)
+	for i := 0; i < 10; i++ {
+		msg1 := <-c
+		fmt.Println(msg1.msg)
+		msg2 := <-c
+		fmt.Println(msg2.msg)
+
+		//its value can be anything.. just a trigger point
+		msg1.wait <- true
+		msg2.wait <- true
 	}
 	fmt.Println("Exiting......")
 }
 
 //https://talks.golang.org/2012/concurrency.slide#28
-func fanIn(input1, input2 <-chan string) <-chan string {
-	c := make(chan string)
+func fanIn(input1, input2 <-chan Message) <-chan Message {
+	c := make(chan Message)
 	go func() {
 		for {
 			c <- <-input1
@@ -33,12 +44,16 @@ func fanIn(input1, input2 <-chan string) <-chan string {
 
 //Generator: function that returns a channel
 //Making use of a closure..
-func boring(msg string) <-chan string { // Returns receive-only channel of strings.
-	c := make(chan string)
+func boring(msg string) <-chan Message {
+	c := make(chan Message)
+
+	//single instance of waitForIt shared across all goroutines.. closures
+	waitForIt := make(chan bool)
 	go func() {
 		for i := 0; ; i++ {
-			c <- fmt.Sprintf("%s %d", msg, i) // Expression to be sent can be any suitable value.
+			c <- Message{fmt.Sprintf("%s %d", msg, i), waitForIt}
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+			<-waitForIt
 		}
 	}()
 	return c // Return the channel to the caller.
