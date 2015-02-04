9e9e6dc1a34e65307145b4fca25a5ffbff7132a3
Vimlesh Sharma <vimlesh.sharma@drillinginfo.com>
2015-02-04 16:18:06 +0530

    Generator Fucntion

--- Concurrency.go
+++ Concurrency.go
@@ -7,16 +7,22 @@ import (
 )
 
 func main() {
-	c := make(chan string)
-	go boring("boring!", c)
+	c := boring("Vimlesh")
 	for i := 0; i <= 5; i++ {
 		fmt.Println(<-c)
 	}
+	fmt.Println("Exiting......")
 }
 
-func boring(msg string, c chan string) {
-	for i := 0; ; i++ {
-		c <- fmt.Sprintf("%s %d", msg, i) // Expression to be sent can be any suitable value.
-		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-	}
+//Generator: function that returns a channel
+//Making use of a closure..
+func boring(msg string) <-chan string { // Returns receive-only channel of strings.
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i) // Expression to be sent can be any suitable value.
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c // Return the channel to the caller.
 }
